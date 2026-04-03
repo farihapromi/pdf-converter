3fb0544c839a13c6fa7257b57c86520824a5ce2f
Fariha Promi <farihapromi2611@gmail.com>
2026-04-03 20:35:44 +0600

    update parser.py

--- converter/parser.py
+++ converter/parser.py
@@ -2,19 +2,19 @@ import pdfplumber
 
 
 def extract_content(pdf_path):
-    all_text = ""
-    tables = []
+    all_text = ""  # Stores full text
+    tables = []  # stores extracted table
 
     with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
+        for page in pdf.pages:  # loop through each page
             # Extract text
-            text = page.extract_text()
+            text = page.extract_text()  # extract raw text from page
             if text:
-                all_text += text + "\n"
+                all_text += text + "\n"  # add text if not empty
 
             # Extract tables
             extracted_tables = page.extract_tables()
             for table in extracted_tables:
-                tables.append(table)
+                tables.append(table)  # stores all tables
 
     return all_text, tables
